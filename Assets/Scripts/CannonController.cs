1bc27ae287ffe855ad5eaaaa6cd4dfe427ffb6e6
Matthew Weil <matt@weil.dev>
2020-10-09 18:54:16 -0400

    setup cannon and projectile

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+	
+	public GameObject cannon;
+	public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+
+		
+		
+        if (Input.GetKey ("up")) {
+			print(cannon.transform.rotation.z);
+			if(cannon.transform.localRotation.z < 0){
+				transform.Rotate(0, 0, 1);
+			}
+		}
+		if (Input.GetKey ("down")) {
+			print(cannon.transform.rotation.z);
+			if(cannon.transform.localRotation.z > -0.7){
+				transform.Rotate(0, 0, -1);
+			}
+			
+		}
+    }
+}
